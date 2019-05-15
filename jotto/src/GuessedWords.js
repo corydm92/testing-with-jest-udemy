c11199ffad0fa714620e6f2784f0dcf8e758d21d
Cory <contact@corymorrissey.com>
2019-05-15 12:07:28 -0700

    added code for GuessedWords.js, two methods of solving in my notes

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -44,3 +44,33 @@ function GuessedWords(props) {
 }
 
 export default GuessedWords;
+
+// My code before following along with the course
+//
+// function GuessedWords(props) {
+//   let content;
+//   if (props.guessedWords.length === 0) {
+//     content = (
+//       <span data-test='guess-instructions'>Try guessing the secret word!</span>
+//     );
+//   } else {
+//     content = (
+//       <table data-test='guessed-words'>
+//         <tr>
+//           <td>Guessed Words</td>
+//           <td>Number Correct</td>
+//         </tr>
+//         {props.guessedWords.map(word => {
+//           let { guessedWord, letterMatchCount } = word;
+//           return (
+//             <tr key={guessedWord} data-test='guessed-word'>
+//               <td>{guessedWord}</td>
+//               <td>{letterMatchCount}</td>
+//             </tr>
+//           );
+//         })}
+//       </table>
+//     );
+//   }
+
+//   return <div data-test='component-guessed-words'>{content} </div>;
