3aaad8e500226fbe12c9398912c1edae59924d0f
Cory <contact@corymorrissey.com>
2019-05-15 15:09:01 -0700

    set up redux

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -18,14 +18,14 @@ function GuessedWords(props) {
 		content = (
 			<div data-test='guessed-words'>
 				<h3>Guessed Words</h3>
-				<table>
-					<thead>
+				<table className='table table-sm'>
+					<thead className='thead-light'>
 						<tr>
 							<th>Guess</th>
 							<th>Matching Letters</th>
 						</tr>
-						<tbody>{guessedWordsRows}</tbody>
 					</thead>
+					<tbody>{guessedWordsRows}</tbody>
 				</table>
 			</div>
 		);
